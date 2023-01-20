b5187b8e2365e9fac9a78f8ab7f7dbc73af85f98
varun gupta <varung735@gmail.com>
2023-01-21 00:37:54 +0530

    added audio player

--- 12_Music_Player/playlist.js
+++ 12_Music_Player/playlist.js
@@ -1,10 +1,12 @@
 const playlist = [
     {
         "id": 0,
+        "song-name": "Brown Munde",
         "audio": "",
-        "album-art": "",
-        "album": "",
-        "artist": "",
-        "label": ""
+        "album-art": "https://i.scdn.co/image/ab67616d0000b273d9a129c4a656a55afff2ca02",
+        "album": "Non-Album Single",
+        "artist": "AP Dhillion, Gminxr, Gurinder Gill, Shinda Kahlon",
+        "label": "Run-Up Records",
+        "released-on": "2020"
     }
 ]
