b0a1befd43debe4a1a45a40a8b7eee54226801d3
varun gupta <varung735@gmail.com>
2023-01-23 23:08:57 +0530

    ui's ready

--- 12_Music_Player/playlist.js
+++ 12_Music_Player/playlist.js
@@ -8,5 +8,75 @@ const playlist = [
         "artist": "AP Dhillion, Gminxr, Gurinder Gill, Shinda Kahlon",
         "label": "Run-Up Records",
         "released-on": "2020"
+    },
+    {
+        "id": 1,
+        "song-name": "I took a pill in ibiza",
+        "audio": "",
+        "album-art": "https://i.scdn.co/image/ab67616d0000b2731db27fa11dbafa67857da8f3",
+        "album": "Non-Album Single",
+        "artist": "Mike Posner, Seeb",
+        "label": "Run-Up Records",
+        "released-on": "2016"
+    },
+    {
+        "id": 2,
+        "song-name": "On and On",
+        "audio": "",
+        "album-art": "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/152/1000x0/on-on-feat-daniel-levi-1586947008-oeI24in2Ga.jpg",
+        "album": "Non-Album Single",
+        "artist": "Cvrtonn, Daniel Levi",
+        "label": "NCS- NoCopyrightSounds",
+        "released-on": "2015"
+    },
+    {
+        "id": 3,
+        "song-name": "Ram Darshan",
+        "audio": "",
+        "album-art": "https://c.saavncdn.com/829/Ram-Setu-Hindi-2022-20220920221433-500x500.jpg",
+        "album": "Ram Setu EP",
+        "artist": "Narci",
+        "label": "Narci",
+        "released-on": "2022"
+    },
+    {
+        "id": 4,
+        "song-name": "Numb",
+        "audio": "",
+        "album-art": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeXS5RFCMeSmtqLB4NjtxAeaSS-mgqZA9rtg&usqp=CAU",
+        "album": "Meteora",
+        "artist": "Linkin Park",
+        "label": "Warner Bros.",
+        "released-on": "sept-2003"
+    },
+    {
+        "id": 4,
+        "song-name": "Leave Out All The Rest",
+        "audio": "",
+        "album-art": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqXxh564rBtDAaUj-wdZbIDwUBygpQ6c5mFA&usqp=CAU",
+        "album": "Minutes To Midnight",
+        "artist": "Linkin-Park",
+        "label": "Warner Bros.",
+        "released-on": "15-July-2008"
+    },
+    {
+        "id": 5,
+        "song-name": "Somewhere I Belong",
+        "audio": "",
+        "album-art": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeXS5RFCMeSmtqLB4NjtxAeaSS-mgqZA9rtg&usqp=CAU",
+        "album": "Meteora",
+        "artist": "Linkin Park",
+        "label": "Warner Bros.",
+        "released-on": "24-feb-2002"
+    },
+    {
+        "id": 6,
+        "song-name": "Zinda",
+        "audio": "",
+        "album-art": "https://i.scdn.co/image/ab67616d0000b2730e26db222796cf4207425abc",
+        "album": "Lootera (2013)",
+        "artist": "Amit Trivedi, Amitabh Bhattacharya",
+        "label": "T-Series",
+        "released-on": "7-June-2013"
     }
 ]
---
+++ 12_Music_Player/scripts.js
@@ -0,0 +1,16 @@
+const musicImg = document.getElementById("music-img");
+const musicName = document.getElementById("music-name");
+const album = document.getElementById("album");
+const releasedOn = document.getElementById("released-on");
+const artist = document.getElementById("artist");
+const label = document.getElementById("label");
+const progressBar = document.getElementById("progress-bar");
+const musicSrc = document.getElementById("music-src");
+const prevBtn = document.getElementById("previous");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+const volumeBar = document.getElementById("volumeBar");
+const albumArt = document.getElementById("pi-album-art");
+const songName = document.getElementById("pi-song-name");
+const songArtist = document.getElementById("pi-song-artist");
+
