4f84001a3747b043c4004e74b545e4bf7323dd33
varun gupta <varung735@gmail.com>
2023-01-27 17:12:25 +0530

    music list working perfectly

--- 12_Music_Player/playlist.js
+++ 12_Music_Player/playlist.js
@@ -1,9 +1,9 @@
-const playlist = [
+ const playlist = [
     {
         "id": 0,
-        "song-name": "Brown Munde",
+        "name": "Brown Munde",
         "audio": "",
-        "album-art": "https://i.scdn.co/image/ab67616d0000b273d9a129c4a656a55afff2ca02",
+        "albumArt": "https://i.scdn.co/image/ab67616d0000b273d9a129c4a656a55afff2ca02",
         "album": "Non-Album Single",
         "artist": "AP Dhillion, Gminxr, Gurinder Gill, Shinda Kahlon",
         "label": "Run-Up Records",
@@ -11,9 +11,9 @@ const playlist = [
     },
     {
         "id": 1,
-        "song-name": "I took a pill in ibiza",
+        "name": "I took a pill in ibiza",
         "audio": "",
-        "album-art": "https://i.scdn.co/image/ab67616d0000b2731db27fa11dbafa67857da8f3",
+        "albumArt": "https://i.scdn.co/image/ab67616d0000b2731db27fa11dbafa67857da8f3",
         "album": "Non-Album Single",
         "artist": "Mike Posner, Seeb",
         "label": "Run-Up Records",
@@ -21,19 +21,19 @@ const playlist = [
     },
     {
         "id": 2,
-        "song-name": "On and On",
+        "name": "On and On",
         "audio": "",
-        "album-art": "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/152/1000x0/on-on-feat-daniel-levi-1586947008-oeI24in2Ga.jpg",
+        "albumArt": "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/152/1000x0/on-on-feat-daniel-levi-1586947008-oeI24in2Ga.jpg",
         "album": "Non-Album Single",
-        "artist": "Cvrtonn, Daniel Levi",
+        "artist": "Cvrtoon, Daniel Levi",
         "label": "NCS- NoCopyrightSounds",
         "released-on": "2015"
     },
     {
         "id": 3,
-        "song-name": "Ram Darshan",
+        "name": "Ram Darshan",
         "audio": "",
-        "album-art": "https://c.saavncdn.com/829/Ram-Setu-Hindi-2022-20220920221433-500x500.jpg",
+        "albumArt": "https://c.saavncdn.com/829/Ram-Setu-Hindi-2022-20220920221433-500x500.jpg",
         "album": "Ram Setu EP",
         "artist": "Narci",
         "label": "Narci",
@@ -41,9 +41,9 @@ const playlist = [
     },
     {
         "id": 4,
-        "song-name": "Numb",
+        "name": "Numb",
         "audio": "",
-        "album-art": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeXS5RFCMeSmtqLB4NjtxAeaSS-mgqZA9rtg&usqp=CAU",
+        "albumArt": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeXS5RFCMeSmtqLB4NjtxAeaSS-mgqZA9rtg&usqp=CAU",
         "album": "Meteora",
         "artist": "Linkin Park",
         "label": "Warner Bros.",
@@ -51,9 +51,9 @@ const playlist = [
     },
     {
         "id": 4,
-        "song-name": "Leave Out All The Rest",
+        "name": "Leave Out All The Rest",
         "audio": "",
-        "album-art": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqXxh564rBtDAaUj-wdZbIDwUBygpQ6c5mFA&usqp=CAU",
+        "albumArt": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqXxh564rBtDAaUj-wdZbIDwUBygpQ6c5mFA&usqp=CAU",
         "album": "Minutes To Midnight",
         "artist": "Linkin-Park",
         "label": "Warner Bros.",
@@ -61,9 +61,9 @@ const playlist = [
     },
     {
         "id": 5,
-        "song-name": "Somewhere I Belong",
+        "name": "Somewhere I Belong",
         "audio": "",
-        "album-art": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeXS5RFCMeSmtqLB4NjtxAeaSS-mgqZA9rtg&usqp=CAU",
+        "albumArt": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeXS5RFCMeSmtqLB4NjtxAeaSS-mgqZA9rtg&usqp=CAU",
         "album": "Meteora",
         "artist": "Linkin Park",
         "label": "Warner Bros.",
@@ -71,12 +71,14 @@ const playlist = [
     },
     {
         "id": 6,
-        "song-name": "Zinda",
+        "name": "Zinda",
         "audio": "",
-        "album-art": "https://i.scdn.co/image/ab67616d0000b2730e26db222796cf4207425abc",
+        "albumArt": "https://i.scdn.co/image/ab67616d0000b2730e26db222796cf4207425abc",
         "album": "Lootera (2013)",
         "artist": "Amit Trivedi, Amitabh Bhattacharya",
         "label": "T-Series",
         "released-on": "7-June-2013"
     }
-]
+];
+
+export default playlist;
--- 12_Music_Player/scripts.js
+++ 12_Music_Player/scripts.js
@@ -1,3 +1,5 @@
+import playlist  from "./playlist.js";
+
 const musicImg = document.getElementById("music-img");
 const musicName = document.getElementById("music-name");
 const album = document.getElementById("album");
@@ -10,7 +12,40 @@ const prevBtn = document.getElementById("previous");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 const volumeBar = document.getElementById("volumeBar");
+const playlistDiv = document.getElementById("playlist-box");
 const albumArt = document.getElementById("pi-album-art");
 const songName = document.getElementById("pi-song-name");
 const songArtist = document.getElementById("pi-song-artist");
 
+// add the songs from playlist in playlist box as play item
+const createPlayItem = (songName, albumArtSrc, songArtist) => {
+    const playItemDiv = document.createElement('div');
+    const albumArtDiv = document.createElement('div');
+    const piSongDetailsDiv = document.createElement('div');
+    const albumArtImg = document.createElement('img');
+    const songNameH4 = document.createElement('h4');
+    const songArtistP = document.createElement('p');
+
+    playItemDiv.classList.add('play-item');
+    albumArtDiv.classList.add('pi-album-art-div');
+    piSongDetailsDiv.classList.add('pi-song-details');
+    songNameH4.classList.add('song-name');
+    songArtistP.classList.add('song-artist');
+    albumArtImg.classList.add('album-art');
+
+    albumArtImg.setAttribute('src', albumArtSrc);
+    songNameH4.innerText = songName;
+    songArtistP.innerText = songArtist;
+
+    playItemDiv.appendChild(albumArtDiv);
+    playItemDiv.appendChild(piSongDetailsDiv);
+    albumArtDiv.appendChild(albumArtImg);
+    piSongDetailsDiv.appendChild(songNameH4);
+    piSongDetailsDiv.appendChild(songArtistP);
+
+    playlistDiv.appendChild(playItemDiv);
+}
+
+playlist.map((song) => {
+    createPlayItem(song.name, song.albumArt, song.artist);
+})
