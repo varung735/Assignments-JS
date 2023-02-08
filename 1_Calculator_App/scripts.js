31854b8b52939c99bb9eb4bb7de33f111f061bed
varun gupta <varung735@gmail.com>
2023-02-08 23:40:16 +0530

    calculator's working fine

--- 1_Calculator_App/scripts.js
+++ 1_Calculator_App/scripts.js
@@ -1,6 +1,8 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".button");
-
+let a;
+let b;
+let operator;
 //functions
 
 //function to insert value into display
@@ -11,6 +13,40 @@ const insertValue = (value) => {
 //function to clear display
 const clearDisplay = () => {
     display.value = 0;
+}
+
+//function to operate on assigned values
+const operation = (a, b, operator) => {
+    if(operator == "+"){
+        operator = "";
+        return a + b;
+    }
+    else if(operator == "-"){
+        operator = "";
+        return a - b;
+    }
+    else if(operator == "*"){
+        operator = "";
+        return a * b;
+    }
+    else{
+        operator = "";
+        return a/b;
+    }
+}
+
+//function to assign the a variable and operator
+const assignA = (id) => {
+    a = display.value;
+    operator = id;
+    display.value = 0;
+}
+
+//function to assign the b variable
+const assignB = () => {
+    b = display.value;
+    let c = operation(parseInt(a), parseInt(b), operator);
+    display.value = c;
 }
 
 //listeners
@@ -29,5 +65,21 @@ buttons.forEach((button) => {
             }
         });
     }
+});
 
+buttons.forEach((button) => {
+    // the conditions doesn't allows the listener to be applied on AC and "equal-to" or "=" button
+    if(button.classList.contains("no-listener") && !button.classList.contains("equal-to") && !button.classList.contains("ac")){
+        button.addEventListener('click', () => {
+            assignA(button.getAttribute('id'));
+        });
+    }
+});
+
+buttons.forEach((button) => {
+    if(button.classList.contains("equal-to")){
+        button.addEventListener('click', () => {
+            assignB();
+        });
+    }
 });
