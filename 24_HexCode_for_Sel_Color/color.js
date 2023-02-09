62f69c994c2a15942a2e675ccf1c570b9051da6b
varun gupta <varung735@gmail.com>
2023-02-09 23:51:21 +0530

    updating colors in colors.js and ui is somewhat ready

---
+++ 24_HexCode_for_Sel_Color/color.js
@@ -0,0 +1,79 @@
+const red = [
+    {"id": 0, "colorName": "Red", "hex": "#D0312D"},
+    {"id": 1, "colorName": "Rose", "hex": "#E3242B"},
+    {"id": 2, "colorName": "Candy", "hex": "D21404"},
+    {"id": 3, "colorName": "Crimson", "hex": "#B90E0A"},
+    {"id": 4, "colorName": "Apple", "hex": "#A91B0D"},
+    {"id": 5, "colorName": "Lipstick", "hex": "#9B1003"},
+    {"id": 6, "colorName": "Berry", "hex": "#7A1712"},
+    {"id": 7, "colorName": "Brick", "hex": "#7E2811"},
+    {"id": 8, "colorName": "Blood", "hex": "#710C04"},
+    {"id": 9, "colorName": "Scarlet", "hex": "#900D09"},
+    {"id": 10, "colorName": "Ruby", "hex": "#900603"}
+];
+
+const blue = [
+    {"id": 0, "colorName": "Arctic", "hex": "#82EEFD"},
+    {"id": 1, "colorName": "Sky", "hex": "#63C5DA"},
+    {"id": 2, "colorName": "Cerulean", "hex": "#0492C2"},
+    {"id": 3, "colorName": "Blue", "hex": "#3944BC"},
+    {"id": 4, "colorName": "Cobalt", "hex": "#1338BE"},
+    {"id": 5, "colorName": "Lapis", "hex": "#2832C2"},
+    {"id": 6, "colorName": "Azure", "hex": "#1520A6"},
+    {"id": 7, "colorName": "Admiral", "hex": "#051094"},
+    {"id": 8, "colorName": "Navy", "hex": "#0A1172"},
+    {"id": 9, "colorName": "Berry", "hex": "#241571"},
+    {"id": 10, "colorName": "Indigo", "hex": "#281E5D"},
+    {"id": 11, "colorName": "Sapphire", "hex": "#52B2Bf"},
+    {"id": 12, "colorName": "Teal", "hex": "#48AAAD"},
+    {"id": 13, "colorName": "Ocean", "hex": "#016064"},
+    {"id": 14, "colorName": "Peacock", "hex": "#022D36"}
+];
+
+const green = [
+    {"id": 0, "colorName": "Mint", "hex": "#99EDC3"},
+    {"id": 1, "colorName": "Seafoam", "hex": "#3DED97"},
+    {"id": 2, "colorName": "Fern", "hex": "5DBB63"},
+    {"id": 3, "colorName": "Parakeet", "hex": "#03C04A"},
+    {"id": 4, "colorName": "Green", "hex": "#3CB043"},
+    {"id": 5, "colorName": "Shamrock", "hex": "#03AC13"},
+    {"id": 6, "colorName": "Emerald", "hex": "#028A0F"},
+    {"id": 7, "colorName": "Chartreuse", "hex": "#B0FC38"},
+    {"id": 8, "colorName": "Lime", "hex": "#AEF359"},
+    {"id": 9, "colorName": "Pear", "hex": "#74B72E"},
+    {"id": 10, "colorName": "Crocodile", "hex": "#607D3B"},
+    {"id": 11, "colorName": "Pickle", "hex": "#597D35"},
+    {"id": 12, "colorName": "Moss", "hex": "466D1D"},
+    {"id": 13, "colorName": "Seaweed", "hex": "#354A21"},
+    {"id": 14, "colorName": "Juniper", "hex": "#3A5311"}
+];
+
+const yellow = [
+    {"id": 0, "colorName": "Chartreuse", "hex": "#DFFE00"},
+    {"id": 1, "colorName": "Lemon", "hex": "#EEFC5e"},
+    {"id": 2, "colorName": "Banana", "hex": "#FEE135"},
+    {"id": 3, "colorName": "Canary", "hex": "#FFEF00"},
+    {"id": 4, "colorName": "Bumblebee", "hex": "#fBE106"},
+    {"id": 5, "colorName": "Cyber", "hex": "#FFD300"},
+    {"id": 6, "colorName": "Tuscany", "hex": "#FDD128"},
+    {"id": 7, "colorName": "Amber", "hex": "#FEBE00"},
+    {"id": 8, "colorName": "Honey", "hex": "#FEC20C"},
+    {"id": 9, "colorName": "Fire", "hex": "#FCA510"},
+    {"id": 10, "colorName": "Gold", "hex": "#F9A603"},
+    {"id": 11, "colorName": "Dijon", "hex": "#C49102"},
+    {"id": 12, "colorName": "Ochre", "hex": "#CC7722"}
+];
+
+const black = [
+
+];
+
+const white = [
+
+];
+
+const grey = [
+
+];
+
+export default colors;
---
+++ 24_HexCode_for_Sel_Color/scripts.js
@@ -0,0 +1,9 @@
+import colors from "./color.js";
+
+const redSec = document.getElementById("red");
+const greenSec = document.getElementById("green");
+const blueSec = document.getElementById("blue");
+const yellowSec = document.getElementById("yellow");
+const whiteSec = document.getElementById("white");
+const blackSec = document.getElementById("black");
+const greySec = document.getElementById("grey");
