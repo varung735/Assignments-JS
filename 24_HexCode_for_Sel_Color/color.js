0d2e95e89285ba33271c9311a53a9024fb2cbbf5
varun gupta <varung735@gmail.com>
2023-02-11 23:24:15 +0530

    color picker complete

--- 24_HexCode_for_Sel_Color/color.js
+++ 24_HexCode_for_Sel_Color/color.js
@@ -1,7 +1,7 @@
-const red = [
+export const red = [
     {"id": 0, "colorName": "Red", "hex": "#D0312D"},
     {"id": 1, "colorName": "Rose", "hex": "#E3242B"},
-    {"id": 2, "colorName": "Candy", "hex": "D21404"},
+    {"id": 2, "colorName": "Candy", "hex": "#D21404"},
     {"id": 3, "colorName": "Crimson", "hex": "#B90E0A"},
     {"id": 4, "colorName": "Apple", "hex": "#A91B0D"},
     {"id": 5, "colorName": "Lipstick", "hex": "#9B1003"},
@@ -12,7 +12,7 @@ const red = [
     {"id": 10, "colorName": "Ruby", "hex": "#900603"}
 ];
 
-const blue = [
+export const blue = [
     {"id": 0, "colorName": "Arctic", "hex": "#82EEFD"},
     {"id": 1, "colorName": "Sky", "hex": "#63C5DA"},
     {"id": 2, "colorName": "Cerulean", "hex": "#0492C2"},
@@ -26,14 +26,13 @@ const blue = [
     {"id": 10, "colorName": "Indigo", "hex": "#281E5D"},
     {"id": 11, "colorName": "Sapphire", "hex": "#52B2Bf"},
     {"id": 12, "colorName": "Teal", "hex": "#48AAAD"},
-    {"id": 13, "colorName": "Ocean", "hex": "#016064"},
-    {"id": 14, "colorName": "Peacock", "hex": "#022D36"}
+    {"id": 13, "colorName": "Ocean", "hex": "#016064"}
 ];
 
-const green = [
+export const green = [
     {"id": 0, "colorName": "Mint", "hex": "#99EDC3"},
     {"id": 1, "colorName": "Seafoam", "hex": "#3DED97"},
-    {"id": 2, "colorName": "Fern", "hex": "5DBB63"},
+    {"id": 2, "colorName": "Fern", "hex": "#5DBB63"},
     {"id": 3, "colorName": "Parakeet", "hex": "#03C04A"},
     {"id": 4, "colorName": "Green", "hex": "#3CB043"},
     {"id": 5, "colorName": "Shamrock", "hex": "#03AC13"},
@@ -43,12 +42,11 @@ const green = [
     {"id": 9, "colorName": "Pear", "hex": "#74B72E"},
     {"id": 10, "colorName": "Crocodile", "hex": "#607D3B"},
     {"id": 11, "colorName": "Pickle", "hex": "#597D35"},
-    {"id": 12, "colorName": "Moss", "hex": "466D1D"},
-    {"id": 13, "colorName": "Seaweed", "hex": "#354A21"},
-    {"id": 14, "colorName": "Juniper", "hex": "#3A5311"}
+    {"id": 12, "colorName": "Moss", "hex": "#466D1D"},
+    {"id": 13, "colorName": "Seaweed", "hex": "#354A21"}
 ];
 
-const yellow = [
+export const yellow = [
     {"id": 0, "colorName": "Chartreuse", "hex": "#DFFE00"},
     {"id": 1, "colorName": "Lemon", "hex": "#EEFC5e"},
     {"id": 2, "colorName": "Banana", "hex": "#FEE135"},
@@ -64,16 +62,54 @@ const yellow = [
     {"id": 12, "colorName": "Ochre", "hex": "#CC7722"}
 ];
 
-const black = [
-
+export const black = [
+    {"id": 0, "colorName": "Black", "hex": "#000000"},
+    {"id": 1, "colorName": "Ebony", "hex": "#080402"},
+    {"id": 2, "colorName": "Crow", "hex": "#0D0907"},
+    {"id": 3, "colorName": "Charcoal", "hex": "#28231D"},
+    {"id": 4, "colorName": "Midnight", "hex": "#040406"},
+    {"id": 5, "colorName": "Ink", "hex": "#070504"},
+    {"id": 6, "colorName": "Raven", "hex": "#050301"},
+    {"id": 7, "colorName": "Oil", "hex": "#050100"},
+    {"id": 8, "colorName": "Grease", "hex": "#0A0806"},
+    {"id": 9, "colorName": "Onyx", "hex": "#030104"},
+    {"id": 10, "colorName": "Pitch", "hex": "#030001"},
+    {"id": 11, "colorName": "Soot", "hex": "#160D08"},
+    {"id": 12, "colorName": "Sable", "hex": "#060606"},
+    {"id": 13, "colorName": "Jet-Black", "hex": "#030303"}
 ];
 
-const white = [
-
+export const white = [
+    {"id": 0, "colorName": "White", "hex": "#ffffff"},
+    {"id": 1, "colorName": "Pearl", "hex": "#FBFCF8"},
+    {"id": 2, "colorName": "Alabaster", "hex": "#FEF9F3"},
+    {"id": 3, "colorName": "Snow", "hex": "#F5FEFD"},
+    {"id": 4, "colorName": "Ivory", "hex": "#FDF6E4"},
+    {"id": 5, "colorName": "Cream", "hex": "#FFFADA"},
+    {"id": 6, "colorName": "Egg-Shell", "hex": "#FFF9E3"},
+    {"id": 7, "colorName": "Cotton", "hex": "#FBFBF9"},
+    {"id": 8, "colorName": "Chiffan", "hex": "#FBFAF2"},
+    {"id": 9, "colorName": "Salt", "hex": "#F8EEEC"},
+    {"id": 10, "colorName": "Lace", "hex": "#F8F2ED"},
+    {"id": 11, "colorName": "Coconut", "hex": "#FFF1E6"},
+    {"id": 12, "colorName": "Linen", "hex": "#F3EAD3"},
+    {"id": 13, "colorName": "Bone", "hex": "#E7DECC"}
 ];
 
-const grey = [
+export const grey = [
+    {"id": 0, "colorName": "Grey", "hex": "#808080"},
+    {"id": 1, "colorName": "Shadow", "hex": "#373737"},
+    {"id": 2, "colorName": "Graphite", "hex": "#594D5B"},
+    {"id": 3, "colorName": "Iron", "hex": "#322D31"},
+    {"id": 4, "colorName": "Pewter", "hex": "#696880"},
+    {"id": 5, "colorName": "Cloud", "hex": "#C5C6D0"},
+    {"id": 6, "colorName": "Silver", "hex": "#ADADC9"},
+    {"id": 7, "colorName": "Smoke", "hex": "#59515E"},
+    {"id": 8, "colorName": "Slate", "hex": "#3E3D53"},
+    {"id": 9, "colorName": "Anchor", "hex": "#41424C"},
+    {"id": 10, "colorName": "Ash", "hex": "#564C4D"},
+    {"id": 11, "colorName": "Porpoise", "hex": "#4D4C5C"},
+    {"id": 12, "colorName": "Dove", "hex": "#7C6E7F"},
+    {"id": 13, "colorName": "Fog", "hex": "#655967"}
 
 ];
-
-export default colors;
--- 24_HexCode_for_Sel_Color/scripts.js
+++ 24_HexCode_for_Sel_Color/scripts.js
@@ -1,9 +1,237 @@
-import colors from "./color.js";
+import { red, blue, green, yellow, white, black, grey } from "./color.js";
 
-const redSec = document.getElementById("red");
-const greenSec = document.getElementById("green");
-const blueSec = document.getElementById("blue");
-const yellowSec = document.getElementById("yellow");
-const whiteSec = document.getElementById("white");
-const blackSec = document.getElementById("black");
-const greySec = document.getElementById("grey");
+const cardHolder = document.querySelectorAll(".card-holder");
+
+//for each method on red array
+red.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    const colorH3 = document.createElement('h3');
+    const colorP = document.createElement('p');
+    
+    colorDiv.classList.add("color-card");
+    colorH3.classList.add("color-name");
+    colorH3.classList.add("hidden");
+    colorP.classList.add("hex-code");
+    colorP.classList.add("hidden");
+    
+    colorH3.innerText = color.colorName.toUpperCase();
+    colorP.innerText = color.hex.toUpperCase();
+
+    colorDiv.appendChild(colorH3);
+    colorDiv.appendChild(colorP);
+
+    if(cardHolder[0].classList.contains("red")){
+        colorDiv.style.backgroundColor = color.hex;
+        cardHolder[0].appendChild(colorDiv);
+
+        colorDiv.addEventListener('mouseenter', () => {
+            colorH3.classList.remove("hidden");
+            colorP.classList.remove("hidden");
+        });
+        colorDiv.addEventListener('mouseleave', () => {
+            colorH3.classList.add("hidden");
+            colorP.classList.add("hidden");
+        });
+    }
+});
+
+//for each method on green array
+green.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    const colorH3 = document.createElement('h3');
+    const colorP = document.createElement('p');
+
+    colorDiv.classList.add("color-card");
+    colorH3.classList.add("color-name");
+    colorH3.classList.add("hidden");
+    colorP.classList.add("hex-code");
+    colorP.classList.add("hidden");
+
+    colorH3.innerText = color.colorName.toUpperCase();
+    colorP.innerText = color.hex.toUpperCase();
+
+    colorDiv.appendChild(colorH3);
+    colorDiv.appendChild(colorP);
+    
+    if(cardHolder[1].classList.contains("green")){
+        colorDiv.style.backgroundColor = color.hex;
+        cardHolder[1].appendChild(colorDiv);
+
+        colorDiv.addEventListener('mouseenter', () => {
+            colorH3.classList.remove("hidden");
+            colorP.classList.remove("hidden");
+        });
+        colorDiv.addEventListener('mouseleave', () => {
+            colorH3.classList.add("hidden");
+            colorP.classList.add("hidden");
+        });
+    }
+});
+
+//for each method on blue array
+blue.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    const colorH3 = document.createElement('h3');
+    const colorP = document.createElement('p');
+
+    colorDiv.classList.add("color-card");
+    colorH3.classList.add("color-name");
+    colorH3.classList.add("hidden");
+    colorP.classList.add("hex-code");
+    colorP.classList.add("hidden");
+
+    colorH3.innerText = color.colorName.toUpperCase();
+    colorP.innerText = color.hex.toUpperCase();
+
+    colorDiv.appendChild(colorH3);
+    colorDiv.appendChild(colorP);
+    
+    if(cardHolder[2].classList.contains("blue")){
+        colorDiv.style.backgroundColor = color.hex;
+        cardHolder[2].appendChild(colorDiv);
+
+        colorDiv.addEventListener('mouseenter', () => {
+            colorH3.classList.remove("hidden");
+            colorP.classList.remove("hidden");
+        });
+        colorDiv.addEventListener('mouseleave', () => {
+            colorH3.classList.add("hidden");
+            colorP.classList.add("hidden");
+        });
+    }
+});
+
+//for each method on yellow array
+yellow.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    const colorH3 = document.createElement('h3');
+    const colorP = document.createElement('p');
+
+    colorDiv.classList.add("color-card");
+    colorH3.classList.add("color-name");
+    colorH3.classList.add("hidden");
+    colorP.classList.add("hex-code");
+    colorP.classList.add("hidden");
+
+    colorH3.innerText = color.colorName.toUpperCase();
+    colorP.innerText = color.hex.toUpperCase();
+
+    colorDiv.appendChild(colorH3);
+    colorDiv.appendChild(colorP);
+    
+    if(cardHolder[3].classList.contains("yellow")){
+        colorDiv.style.backgroundColor = color.hex;
+        cardHolder[3].appendChild(colorDiv);
+
+        colorDiv.addEventListener('mouseenter', () => {
+            colorH3.classList.remove("hidden");
+            colorP.classList.remove("hidden");
+        });
+        colorDiv.addEventListener('mouseleave', () => {
+            colorH3.classList.add("hidden");
+            colorP.classList.add("hidden");
+        });
+    }
+});
+
+//for each method on black array
+black.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    const colorH3 = document.createElement('h3');
+    const colorP = document.createElement('p');
+
+    colorH3.style.color = "#fff";
+    colorP.style.color = "#fff";
+
+    colorDiv.classList.add("color-card");
+    colorH3.classList.add("color-name");
+    colorH3.classList.add("hidden");
+    colorP.classList.add("hex-code");
+    colorP.classList.add("hidden");
+
+    colorH3.innerText = color.colorName.toUpperCase();
+    colorP.innerText = color.hex.toUpperCase();
+
+    colorDiv.appendChild(colorH3);
+    colorDiv.appendChild(colorP);
+    
+    if(cardHolder[4].classList.contains("black")){
+        colorDiv.style.backgroundColor = color.hex;
+        cardHolder[4].appendChild(colorDiv);
+
+        colorDiv.addEventListener('mouseenter', () => {
+            colorH3.classList.remove("hidden");
+            colorP.classList.remove("hidden");
+        });
+        colorDiv.addEventListener('mouseleave', () => {
+            colorH3.classList.add("hidden");
+            colorP.classList.add("hidden");
+        });
+    }
+});
+
+//for each method on white array
+white.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    const colorH3 = document.createElement('h3');
+    const colorP = document.createElement('p');
+
+    colorDiv.classList.add("color-card");
+    colorH3.classList.add("color-name");
+    colorH3.classList.add("hidden");
+    colorP.classList.add("hex-code");
+    colorP.classList.add("hidden");
+
+    colorH3.innerText = color.colorName.toUpperCase();
+    colorP.innerText = color.hex.toUpperCase();
+
+    colorDiv.appendChild(colorH3);
+    colorDiv.appendChild(colorP);
+    
+    if(cardHolder[5].classList.contains("white")){
+        colorDiv.style.backgroundColor = color.hex;
+        cardHolder[5].appendChild(colorDiv);
+
+        colorDiv.addEventListener('mouseenter', () => {
+            colorH3.classList.remove("hidden");
+            colorP.classList.remove("hidden");
+        });
+        colorDiv.addEventListener('mouseleave', () => {
+            colorH3.classList.add("hidden");
+            colorP.classList.add("hidden");
+        });
+    }
+});
+
+//for each method on grey array
+grey.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    const colorH3 = document.createElement('h3');
+    const colorP = document.createElement('p');
+
+    colorDiv.classList.add("color-card");
+    colorH3.classList.add("color-name");
+    colorH3.classList.add("hidden");
+    colorP.classList.add("hex-code");
+    colorP.classList.add("hidden");
+
+    colorH3.innerText = color.colorName.toUpperCase();
+    colorP.innerText = color.hex.toUpperCase();
+
+    colorDiv.appendChild(colorH3);
+    colorDiv.appendChild(colorP);
+    
+    if(cardHolder[6].classList.contains("grey")){
+        colorDiv.style.backgroundColor = color.hex;
+        cardHolder[6].appendChild(colorDiv);
+
+        colorDiv.addEventListener('mouseenter', () => {
+            colorH3.classList.remove("hidden");
+            colorP.classList.remove("hidden");
+        });
+        colorDiv.addEventListener('mouseleave', () => {
+            colorH3.classList.add("hidden");
+            colorP.classList.add("hidden");
+        });
+    }
+});
